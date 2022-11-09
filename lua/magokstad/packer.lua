6ac7290691adcf5ebe15afea15673f7a32c7e525
Magnus O <magokstad@gmail.com>
2022-11-09 02:07:23 +0100

    lualine runs on startup

--- lua/magokstad/packer.lua
+++ lua/magokstad/packer.lua
@@ -29,7 +29,10 @@ return require('packer').startup(function(use)
     }
     use {
       'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+      config = function()
+        require("lualine").setup()
+      end,
     }
     use {
       'goolord/alpha-nvim',
